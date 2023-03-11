660ba62b146aabc63706467ba9d814b9a1e666a2
Alexander Bonnet <alex.bonnet.us@gmail.com>
2023-03-11 01:16:21 +0100

    Fix formatting

--- core/tests/test_datamodels.py
+++ core/tests/test_datamodels.py
@@ -3,7 +3,7 @@ import math
 from core.datamodels import CircleV0
 
 
-def test_point() -> None:
+def test_circle() -> None:
     circle = CircleV0(x=5, r=6)
 
     assert circle.area == math.pi * 6 * 6
